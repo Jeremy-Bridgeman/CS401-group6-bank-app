12fb297075c81146681648ed03d2d8e810155e8e
kaylasimus <kaylasimus@gmail.com>
2026-04-23 02:16:40 -0700

    readded CheckingAccount and CreditAccount files

---
+++ code/bankapp/CreditAccount.java
@@ -0,0 +1,17 @@
+package bankapp;
+
+public class CreditAccount extends Account {
+
+	private double creditLimit;
+	private double interestRate;
+	
+	public void makePayment(double amount) {
+		
+	}
+	
+	public double charge(double amount) {
+		
+		double total = 0.0;
+		return total;
+	}
+}
