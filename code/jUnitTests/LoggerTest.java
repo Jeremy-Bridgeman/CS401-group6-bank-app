919a42d792e8dabd0a0730e2b11a2c8db3fa6dba
Jeremy Bridgeman <bridgemanjeremy@yahoo.com>
2026-04-25 09:58:23 -0700

    updated tests for new account format

--- code/jUnitTests/LoggerTest.java
+++ code/jUnitTests/LoggerTest.java
@@ -20,6 +20,8 @@ class LoggerTest {
 
 	private Logger logger;
 	private String originalFilename;
+	private static final String ACCOUNT_KEY_1 = "Jeremy Tester|ACC-1001";
+	private static final String ACCOUNT_KEY_2 = "Sam Tester|ACC-2002";
 
 	@TempDir
 	Path tempDir;
@@ -48,7 +50,7 @@ class LoggerTest {
 
 	@Test
 	void testLogEventAddsLog() {
-		Log log = new Log(Log.TRANSACTION_TYPE.DEPOSIT, "deposit test", 100.0);
+		Log log = new Log(Log.TRANSACTION_TYPE.DEPOSIT, "deposit test", 100.0, ACCOUNT_KEY_1);
 
 		logger.logEvent(log);
 
@@ -57,11 +59,12 @@ class LoggerTest {
 		assertEquals(Log.TRANSACTION_TYPE.DEPOSIT, logs.get(0).getType());
 		assertEquals("deposit test", logs.get(0).getComment());
 		assertEquals(100.0, logs.get(0).getAmount(), 0.0001);
+		assertEquals(ACCOUNT_KEY_1, logs.get(0).getAccountKey());
 	}
 
 	@Test
 	void testGetLogsReturnsCopy() {
-		logger.logEvent(new Log(Log.TRANSACTION_TYPE.OTHER, "copy test", 1.0));
+		logger.logEvent(new Log(Log.TRANSACTION_TYPE.OTHER, "copy test", 1.0, ACCOUNT_KEY_1));
 
 		ArrayList<Log> logs = logger.getLogs();
 		logs.clear();
@@ -71,7 +74,7 @@ class LoggerTest {
 
 	@Test
 	void testSaveLogsWritesFile() throws IOException {
-		logger.logEvent(new Log(Log.TRANSACTION_TYPE.WITHDRAWAL, "save test", 25.0));
+		logger.logEvent(new Log(Log.TRANSACTION_TYPE.WITHDRAWAL, "save test", 25.0, ACCOUNT_KEY_1));
 		logger.saveLogs();
 
 		File file = new File(logger.getFilename());
@@ -81,7 +84,7 @@ class LoggerTest {
 
 	@Test
 	void testReloadLogsReadsSavedFile() {
-		logger.logEvent(new Log(Log.TRANSACTION_TYPE.TRANSFER, "reload test", 75.5));
+		logger.logEvent(new Log(Log.TRANSACTION_TYPE.TRANSFER, "reload test", 75.5, ACCOUNT_KEY_1));
 		logger.saveLogs();
 
 		logger.clearLogs();
@@ -94,14 +97,15 @@ class LoggerTest {
 		assertEquals(Log.TRANSACTION_TYPE.TRANSFER, logs.get(0).getType());
 		assertEquals("reload test", logs.get(0).getComment());
 		assertEquals(75.5, logs.get(0).getAmount(), 0.0001);
+		assertEquals(ACCOUNT_KEY_1, logs.get(0).getAccountKey());
 	}
 
 	@Test
 	void testReloadLogsSkipsBadLines() throws IOException {
 		FileWriter writer = new FileWriter(logger.getFilename());
-		writer.write("1000|DEPOSIT|good line|50.0" + System.lineSeparator());
+		writer.write("1000|DEPOSIT|good line|50.0|" + ACCOUNT_KEY_1 + System.lineSeparator());
 		writer.write("bad line here" + System.lineSeparator());
-		writer.write("2000|ERROR|another good line|10.0" + System.lineSeparator());
+		writer.write("2000|ERROR|another good line|10.0|" + ACCOUNT_KEY_2 + System.lineSeparator());
 		writer.close();
 
 		logger.reloadLogs();
@@ -110,12 +114,14 @@ class LoggerTest {
 		assertEquals(2, logs.size());
 		assertEquals(Log.TRANSACTION_TYPE.DEPOSIT, logs.get(0).getType());
 		assertEquals(Log.TRANSACTION_TYPE.ERROR, logs.get(1).getType());
+		assertEquals(ACCOUNT_KEY_1, logs.get(0).getAccountKey());
+		assertEquals(ACCOUNT_KEY_2, logs.get(1).getAccountKey());
 	}
 
 	@Test
 	void testClearLogsRemovesAllLogs() {
-		logger.logEvent(new Log(Log.TRANSACTION_TYPE.DEPOSIT, "a", 1.0));
-		logger.logEvent(new Log(Log.TRANSACTION_TYPE.WITHDRAWAL, "b", 2.0));
+		logger.logEvent(new Log(Log.TRANSACTION_TYPE.DEPOSIT, "a", 1.0, ACCOUNT_KEY_1));
+		logger.logEvent(new Log(Log.TRANSACTION_TYPE.WITHDRAWAL, "b", 2.0, ACCOUNT_KEY_1));
 
 		assertEquals(2, logger.getLogs().size());
 
